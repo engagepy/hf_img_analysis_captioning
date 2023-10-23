e1b3e7333c8941869747e54327af0169d6bae7c8
Zorawar Purohit <42845567+engagepy@users.noreply.github.com>
2023-10-24 05:15:45 +0530

    refactored with streamlit, error messages supressed

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import os
-from dotenv import load_dotenv, find_dotenv
 import logging
+from dotenv import load_dotenv, find_dotenv
 from transformers import pipeline, VitsTokenizer, VitsModel, set_seed, logging as transformers_logging
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 import torch
 import scipy
+import streamlit as st
 
 logging.basicConfig(level=logging.ERROR)
 transformers_logging.set_verbosity_error()
@@ -19,7 +20,7 @@ key_ai = os.getenv("OPAI")
 #img2text
 def img2text(url):
     image_to_text = pipeline("image-to-text", model="Salesforce/blip-image-captioning-base")
-    text = image_to_text(url)[0]#['generated_text']
+    text = image_to_text(url, max_new_tokens=100)[0]#['generated_text']
     print(text)
     return text
 
@@ -72,8 +73,35 @@ def text_to_speech(text):
     return waveform
 
 
-text_to_speech(generate_story(img2text("Beaches-of-Havelock...-1024x769.jpg")))
+#text_to_speech(generate_story(img2text("Beaches-of-Havelock...-1024x769.jpg")))
 
 #You can fine-tune the `waveform.numpy() * <123123>` parameter. While doing so resort to function below.
 
 #text_to_speech("Let us test this line out loud. There is a question ? A moment, and a time to speak fast so that one can get the meaning across !")
+
+def main():
+    image_file_types = ["jpg", "jpeg", "png"]
+
+    st.set_page_config(page_title="img 2 caption", page_icon="")
+    st.header("Get Caption from Image")
+    
+    uploaded_file = st.file_uploader("Choose an image...", type=image_file_types)
+    
+    if uploaded_file is not None:
+        bytes_data = uploaded_file.getvalue()
+        with open(uploaded_file.name, "wb") as file:
+            file.write(bytes_data)
+            st.image(uploaded_file, caption='Uploaded Image.', use_column_width=True)
+        
+        scenario = img2text(uploaded_file.name)
+        story = generate_story(scenario)
+        text_to_speech(story)
+
+        with st.expander("scenario"):
+            st.write(scenario)
+
+        with st.expander("caption"):
+            st.write(story)
+
+if __name__=='__main__':
+    main()
