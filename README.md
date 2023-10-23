72673e53ec4051d9d9f39edf666af5c2db929483
Zorawar Purohit <42845567+engagepy@users.noreply.github.com>
2023-10-24 01:35:32 +0530

    hugging face image analysis and captioning script

---
+++ README.md
@@ -0,0 +1,35 @@
+# Image Analysis and Captioning for Instagram
+
+## Using HuggingFace's [transformers] library to generate captions for
+
+- Image Analysis
+- Instagram posts
+
+## Installation
+
+- Clone the repository `git clone https://github.com/engagepy/hf_img_analysis_captioning.git`
+- Install the requirements `pip install -r requirements.txt`
+- Create .env file and add the following variables
+
+```
+HF_TOKEN=
+OPENAI_API_KEY=
+```
+
+## Usage
+
+- Put an image in the working directory
+- Assign it the function in app.py `generate_story(img2text("<Your-Imag-Name.Extension>"))`
+- Run the script `python app.py`
+
+
+### Enjoy
+
+### Special thanks to the giants on whose shoulders we progress compoundingly in computing. 
+
+- [HuggingFace]
+- [OpenAI]
+- [Streamlit]
+- [PyTorch]
+- [TensorFlow]
+- [LangChain]
---
+++ app.py
@@ -0,0 +1,38 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+from transformers import pipeline
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+load_dotenv(find_dotenv())
+
+# Use a pipeline as a high-level helper
+key_ai = os.getenv("OPAI")
+
+#img2text
+def img2text(url):
+    image_to_text = pipeline("image-to-text", model="Salesforce/blip-image-captioning-base")
+    text = image_to_text(url)[0]#['generated_text']
+    print(text)
+    return text
+
+#llm
+def generate_story(scenario):
+    template = """
+    You are a instagram image captioning expert.
+    You can generate smart, witty captions for images on instagram.
+    Never be offensive or excesively sarcastic or arrogant. 
+
+    CONTEXT = {scenario}
+    STORY:
+"""
+    prompt = PromptTemplate.from_template(template)
+    
+    llm = LLMChain (llm=ChatOpenAI(model_name="gpt-4"), prompt=prompt, verbose=True)
+    story = llm.predict(scenario=scenario)
+
+    print(story)
+    return story
+
+generate_story(img2text("test3.jpeg"))
+#text-to-speech
